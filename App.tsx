11526bb61cd3d9bc958ab34cc7f87c6859d07d22
vallezw <v.zwerschke@mail.de>
2022-05-28 23:43:49 +0200

    Finish up bottom bar icons and screen navigation

--- App.tsx
+++ App.tsx
@@ -10,10 +10,10 @@ import ComposersScreen from "./src/screens/ComposersScreen";
 import SheetsScreen from "./src/screens/SheetsScreen";
 import SettingsScreen from "./src/screens/SettingsScreen";
 import CollectionsScreen from "./src/screens/CollectionsScreen";
-import { HomeIconSvg } from "./src/icons/HomeIconSvg";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from "react-native-vector-icons/Ionicons"
 import { colors, globalStyles } from "./src/constants/GlobalStyleSheet";
+import { TabScreenText}  from "./src/components/utils/TabScreenText";
 
 const Tab = createBottomTabNavigator()
 
@@ -34,21 +34,48 @@ export default function App() {
           <Tab.Screen name="Home" component={HomeScreen} 
           options= {{
             tabBarLabel: ({focused} ) => (
-             <Text style={{
-               paddingLeft: 17,
-               paddingTop: 6,
-               color: focused ? colors.PRIMARY_LIGHT : colors.GRAY7, 
-               ...globalStyles.nunitoSansBodySmall
-             }}>Home</Text> 
+             <TabScreenText focused={focused} label="Home"/> 
             ),
             tabBarIcon: ({ focused, size }) => (
-            <Ionicons name="home" color={focused ? colors.PRIMARY_LIGHT : colors.GRAY7} size={size} outlined />
+            <Ionicons name="home" color={focused ? colors.PRIMARY_LIGHT : colors.GRAY8} size={size}/>
           ),
           }}/>
-          <Tab.Screen name="Sheets" component={SheetsScreen} />
-          <Tab.Screen name="Composers" component={CounterScreen} />
-          <Tab.Screen name="Collections" component={CollectionsScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
+          <Tab.Screen name="Sheets" component={SheetsScreen} 
+            options={{
+              tabBarLabel: ({focused} ) => (
+              <TabScreenText focused={focused} label="Sheets"/> 
+              ),
+            tabBarIcon: ({ focused, size }) => (
+              <MaterialCommunityIcons name="library" color={focused ? colors.PRIMARY_LIGHT : colors.GRAY8} size={size + 3}/>
+            ),}}
+          />
+          <Tab.Screen name="Composers" component={CounterScreen} 
+          options={{
+              tabBarLabel: ({focused} ) => (
+              <TabScreenText focused={focused} label="Composers"/> 
+              ),
+            tabBarIcon: ({ focused, size }) => (
+              <Ionicons name="people" color={focused ? colors.PRIMARY_LIGHT : colors.GRAY8} size={size + 3}/>
+            ),}}
+          />
+          <Tab.Screen name="Collections" component={CollectionsScreen} 
+          options={{
+              tabBarLabel: ({focused} ) => (
+              <TabScreenText focused={focused} label="Collections"/> 
+              ),
+            tabBarIcon: ({ focused, size }) => (
+              <Ionicons name="md-bookmarks" color={focused ? colors.PRIMARY_LIGHT : colors.GRAY8} size={size - 3}/>
+            ),}}
+          />
+          <Tab.Screen name="Settings" component={SettingsScreen} 
+          options={{
+              tabBarLabel: ({focused} ) => (
+              <TabScreenText focused={focused} label="Settings"/> 
+              ),
+            tabBarIcon: ({ focused, size }) => (
+              <Ionicons name="settings" color={focused ? colors.PRIMARY_LIGHT : colors.GRAY8} size={size}/>
+            ),}}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </Provider>
---
+++ src/components/utils/TabScreenText.tsx
@@ -0,0 +1,20 @@
+import { View, Text, Button } from "react-native";
+import { colors, globalStyles } from "../../constants/GlobalStyleSheet";
+
+interface TabScreenTextProps {
+	focused: boolean,
+	label: string
+}
+
+export function TabScreenText({focused, label}: TabScreenTextProps) {
+  return (
+	<Text style={{
+               paddingLeft: 23,
+               paddingTop: 6,
+               color: focused ? colors.PRIMARY_LIGHT : colors.GRAY7, 
+               ...globalStyles.nunitoSansBodySmall
+    }}>{label}</Text> 
+  )
+}
+
+export default TabScreenText
