5a5eac089e7799d254d146ccfff1c09d3c0bf70c
Valentin Zwerschke <v.zwerschke@mail.de>
2022-06-06 00:20:59 +0200

    Ignore ViewPropTypes warning

--- App.tsx
+++ App.tsx
@@ -26,8 +26,7 @@ export default function App() {
   axios.defaults.baseURL = "http://192.168.0.65:8080/api";
 
   // Ignore ViewPropTypes warning
-  //LogBox.ignoreLogs(["ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'."]); 
-  //LogBox.ignoreAllLogs()
+  LogBox.ignoreLogs(["ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'."]); 
 
   if (!loadFonts()) {
     return (
